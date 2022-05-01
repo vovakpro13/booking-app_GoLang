bbab7daad65031e5bceb0f7f2cf0fa384a777f8a
fron1 <vovakpro13@gmail.com>
2022-05-01 18:22:39 +0300

    - var, const - Printf

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello world")
+	const name = "Go Conference"
+	const tickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("Welcome to %v booking application\n", name)
+	fmt.Printf("We have a total of %v tickets and %v are still available.\n", tickets, remainingTickets)
+	fmt.Println("Get your your tickets here to attend")
 }
