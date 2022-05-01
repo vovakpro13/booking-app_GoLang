d8e863f889fb95f04793643fac52da6b5ad63df9
fron1 <vovakpro13@gmail.com>
2022-05-01 18:38:10 +0300

    string, int, uint

--- main.go
+++ main.go
@@ -3,11 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	const name = "Go Conference"
-	const tickets = 50
-	var remainingTickets = 50
+	name := "Go Conference"
+	const tickets uint8 = 50
+	var remainingTickets uint8 = 50
+
+	fmt.Printf("name type is %T, remainingTickets type is %T\n", name, remainingTickets)
 
 	fmt.Printf("Welcome to %v booking application\n", name)
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", tickets, remainingTickets)
 	fmt.Println("Get your your tickets here to attend")
+
+	var userName string
+	var userTickets int
+
+	userName = "Volodymyr"
+	userTickets = 7
+
+	fmt.Printf("User %v want to buy %v tickets", userName, userTickets)
+
 }
