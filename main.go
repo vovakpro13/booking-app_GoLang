f2c5138158c9e9873a1e0f2112c0d1be60815b3a
fron1 <vovakpro13@gmail.com>
2022-05-01 18:54:52 +0300

    input first-last name, email, count of tickets

--- main.go
+++ main.go
@@ -13,12 +13,26 @@ func main() {
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", tickets, remainingTickets)
 	fmt.Println("Get your your tickets here to attend")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
 
-	userName = "Volodymyr"
-	userTickets = 7
+	fmt.Println("What is your first name? ")
+	fmt.Scan(&firstName)
 
-	fmt.Printf("User %v want to buy %v tickets", userName, userTickets)
+	fmt.Println("What is your last name? ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("What is your email? ")
+	fmt.Scan(&email)
+
+	fmt.Println("How many tickets do you want to book? ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf(
+		"Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v\n",
+		firstName, lastName, userTickets, email,
+	)
 
 }
