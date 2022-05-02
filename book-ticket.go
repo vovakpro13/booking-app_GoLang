24246859e2f1a81d1fa95d444acd182eeb18f478
fron1 <vovakpro13@gmail.com>
2022-05-02 08:54:48 +0300

    added goroutine

--- book-ticket.go
+++ book-ticket.go
@@ -3,6 +3,7 @@ package main
 import (
 	"booking-app/helper"
 	"fmt"
+	"time"
 )
 
 func bookTicket(
@@ -20,6 +21,9 @@ func bookTicket(
 
 	bookings = append(bookings, userData)
 
+	wg.Add(1)
+	go sendTicket(userData)
+
 	fmt.Printf("%v \n", bookings)
 	fmt.Printf(
 		"Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v\n",
@@ -28,4 +32,15 @@ func bookTicket(
 
 	fmt.Printf("%v tickets remaining for %v \n", remainingTickets, Conference)
 	fmt.Printf("These are all first names of our visitors: %v \n", helper.GetFirstNames(bookings))
+}
+
+func sendTicket(user helper.UserData) {
+	time.Sleep(10 * time.Second)
+
+	ticket := fmt.Sprintf("%v tickets for %v %v", user.Tickets, user.FirstName, user.LastName)
+
+	fmt.Println("###########")
+	fmt.Printf("Send ticket:\n %v \nto email address %v\n", ticket, user.Email)
+	fmt.Println("###########")
+	wg.Done()
 }
